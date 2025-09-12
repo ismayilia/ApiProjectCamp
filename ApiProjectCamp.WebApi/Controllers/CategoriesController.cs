f130e97df5b81b00156b9f2ffc969650b1bf95ff
Ismayil Afandizada <ismayilia@code.edu.az>
2025-09-12 09:28:24 +0400

    added project

---
+++ ApiProjectCamp.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,61 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+    [Route("api[controller]/[action]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+		public CategoriesController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult CategoryList()
+		{
+			var values = _context.Categories.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateCategory(Category category)
+		{
+			_context.Categories.Add(category);
+			_context.SaveChanges();
+			return Ok("category create success");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteCategory(int id)
+		{
+			//var value = _context.Categories.FirstOrDefault(m=> m.CategoryId==id);
+			var value = _context.Categories.Find(id);
+			_context.Categories.Remove(value);
+			_context.SaveChanges();
+			return Ok("Category");
+		}
+
+		[HttpGet]
+		public IActionResult GetCategory([FromQuery] int id) //burada query yazsaqda olar yazmasaqda,
+			//Route-da "[action]" veya [HttpGet("GetCategory")] veya
+			//["hansisaad"] ve ya [HttpGet("[action]")] olduqda [FromQuery] yazmasaqda olar
+		{
+			var value = _context.Categories.Find(id);
+			return Ok(value);
+		}
+
+		[HttpPut]
+		public IActionResult PutCategory(Category category) 
+		{
+			_context.Categories.Update(category);
+			_context.SaveChanges();
+			return Ok("Update success");
+		}
+	}
+}
---
+++ ApiProjectCamp.WebApi/Controllers/ChefController.cs
@@ -0,0 +1,59 @@
+﻿using ApiProjectCamp.WebApi.Context;
+using ApiProjectCamp.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectCamp.WebApi.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class ChefController : ControllerBase
+	{
+		private readonly ApiContext _context;
+
+		public ChefController(ApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IActionResult ChefList()
+		{
+			var values = _context.Chefs.ToList();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateChef(Chef chef)
+		{
+			_context.Chefs.Add(chef);
+			_context.SaveChanges();
+			return Ok("Chef add success!");
+		}
+
+		[HttpDelete]
+		public IActionResult DeleteChef (int id)
+		{
+			var value = _context.Chefs.Find(id);
+			_context.Remove(value);
+			_context.SaveChanges();
+			return Ok("Delete Chef success");
+		}
+
+		[HttpGet("GetChef")]
+		public IActionResult GetChef(int id)
+		{
+			//var value = _context.Chefs.Find(id);
+			//return Ok(value);
+			return Ok(_context.Chefs.Find(id));
+		}
+
+		[HttpPut]
+		public IActionResult UpdateChef(Chef chef)
+		{
+			_context.Chefs.Update(chef);
+			_context.SaveChanges();
+			return Ok("Update is success!");
+		}
+	}
+}
