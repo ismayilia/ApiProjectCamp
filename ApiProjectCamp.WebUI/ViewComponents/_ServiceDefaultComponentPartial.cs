398d06ecc89eae026ff1d542d6364ea0f5289f8e
Ismayil Afandizada <ismayilia@code.edu.az>
2026-03-31 10:29:30 +0400

    added WhyChooseYummy section in admin

--- ApiProjectCamp.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ ApiProjectCamp.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -16,7 +16,7 @@ namespace ApiProjectCamp.WebUI.ViewComponents
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7256/api/Service/ServiceList");
+			var responseMessage = await client.GetAsync("https://localhost:7256/api/Services/ServiceList");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
