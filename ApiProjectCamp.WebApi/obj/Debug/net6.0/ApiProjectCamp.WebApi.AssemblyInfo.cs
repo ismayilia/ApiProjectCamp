11b53851ceab4593f5bd4de0b5b19abd55650a76
Ismayil Afandizada <ismayilia@code.edu.az>
2026-07-27 10:12:24 +0400

    added signalr sendmessage method

--- ApiProjectCamp.WebApi/obj/Debug/net6.0/ApiProjectCamp.WebApi.AssemblyInfo.cs
+++ ApiProjectCamp.WebApi/obj/Debug/net6.0/ApiProjectCamp.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProjectCamp.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cb198d355531886c2dbb3e1cb0931aec822e28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98d257a2b53072ef12721de75c55f62c0f6341cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProjectCamp.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProjectCamp.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ApiProjectCamp.WebUI/Models/ChatHub.cs
+++ ApiProjectCamp.WebUI/Models/ChatHub.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace ApiProjectCamp.WebUI.Models
 {
 	public class ChatHub : Hub
 	{
 		private const string apikey = "";
-		private const string model = "gpt-4o-mini";
+		private const string modelGpt = "gpt-4o-mini";
 		private readonly IHttpClientFactory _httpClientFactory;
 
 		public ChatHub(IHttpClientFactory httpClientFactory)
@@ -36,7 +39,32 @@ namespace ApiProjectCamp.WebUI.Models
 
 		public async Task SendMessage(string userMessage)
 		{
+			await Clients.Caller.SendAsync("ReceiveUserEcho", userMessage);
+			var history = _history[Context.ConnectionId];
+			history.Add(new() { ["role"] = "user", ["content"] = userMessage });
+			await StreamOpenAI(history, Context.ConnectionAborted);
+		}
 
+		public async Task StreamOpenAI(List<Dictionary<string, string>> history, CancellationToken cancellationToken)
+		{
+			var client = _httpClientFactory.CreateClient("openai");
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apikey);
+			var payload = new
+			{
+				model = modelGpt,
+				messages = history,
+				stream = true,
+				temperature = 0.2
+			};
+
+			using var req = new HttpRequestMessage(HttpMethod.Post, "v1/chat/completions");
+			req.Content = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "applicaton/json");
+
+			using var resp = await client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+			resp.EnsureSuccessStatusCode();
+
+			using var stream = await resp.Content.ReadAsStreamAsync(cancellationToken);
+			using var reader = new StreamReader(stream);
 		}
 	}
 }
